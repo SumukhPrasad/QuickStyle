e323493472eba43665b8e6eb0b02060de9a10629
Sumukh Prasad <sumukhprasad@icloud.com>
2021-10-24 09:19:50 +0530

    First commit.

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	
+</script>
+
+<svelte:head>
+	<title>QuickStyle</title>
+</svelte:head>
+
+<main>
+	
+</main>
+
+<style>
+	
+</style>
---
+++ src/StyleEditor.svelte
@@ -0,0 +1,11 @@
+<script>
+	
+</script>
+
+<main>
+     <h1>In Progress.</h1>
+</main>
+
+<style>
+     
+</style>
