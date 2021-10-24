2acacae7c04c9451605f6e23e9e796c6964c56a2
Sumukh Prasad <sumukhprasad@icloud.com>
2021-10-24 18:11:35 +0530

    Bug fixes, changed the design a bit.

--- src/StyleEditor.svelte
+++ src/StyleEditor.svelte
@@ -1,67 +1,67 @@
 <script>
 	import { fly } from "svelte/transition";
-     import { activeEditors } from './store.js';
-     function remove() {
+	import { activeEditors } from './store.js';
+	function remove() {
 		$activeEditors = $activeEditors.filter(function(value, index, arr){ 
 			if (value.id != activeEditor.id) return value;
 		});
 		console.log($activeEditors);
 	}
-     var rules = '';
-     var className = '';
-     export let activeEditor;
+	var rules = '';
+	var className = '';
+	export let activeEditor;
 </script>
 
 
 <li in:fly="{{ x: 900, duration: 200 }}">
-     <div class="classNameAndEndBracketDiv">
-          <span style="line-height: 30px;">.<input type="text" placeholder="class-name" bind:value={className}> {'{'}</span>
-          <button on:click={remove} class="deletebutton">Delete</button>
-     </div>
-     <div>
-          <textarea placeholder="property: value;" bind:value={rules} resizable={false}></textarea>
-     </div>
-     <div class="classNameAndEndBracketDiv">
-          <span>{'}'}</span>
-     </div>
+	<div class="classNameAndEndBracketDiv">
+		<span style="line-height: 30px;">.<input type="text" placeholder="class-name" bind:value={className}> {'{'}</span>
+		<button on:click={remove} class="deletebutton">Delete</button>
+	</div>
+	<div>
+		<textarea placeholder="property: value;" bind:value={rules} resizable={false}></textarea>
+	</div>
+	<div class="classNameAndEndBracketDiv">
+		<span>{'}'}</span>
+	</div>
 </li>
 
 <style>
 	textarea {
 		resize: none;
-          font-family: 'Courier New', Courier, monospace;
+		font-family: 'Courier New', Courier, monospace;
 		background-color: #222;
 		color: #fff;
-          border-color: #222;
-          width: 100%;
-          font-size: 14px;
-          font-weight: 500;
-          line-height: 20px;
+		border-color: #222;
+		width: 100%;
+		font-size: 14px;
+		font-weight: 500;
+		line-height: 20px;
 	}
-     li {
-          color: #ddd;
-          border-bottom: 1px solid rgba(255, 255, 255, 0.25);
-          padding: 10px;
-          width: 300px;
-     }
-     input {
-          font-size: 14px;
-          font-weight: 500;
-          line-height: 20px;
-          white-space: nowrap;
-          vertical-align: middle;
-          border: 1px solid;
-          border-radius: 6px;
-          height: 30px;
-          border: 0;
-          background-color: #222;
-          color: #fff;
-          width: 200px;
-          font-family: 'Courier New', Courier, monospace;
-     }
-     .deletebutton {
-          background-color: #f00;
-          color: #fff;
-          float: right;
-     }
+	li {
+		color: #ddd;
+		border-bottom: 1px solid rgba(255, 255, 255, 0.25);
+		padding: 10px;
+		width: 300px;
+	}
+	input {
+		font-size: 14px;
+		font-weight: 500;
+		line-height: 20px;
+		white-space: nowrap;
+		vertical-align: middle;
+		border: 1px solid;
+		border-radius: 6px;
+		height: 30px;
+		border: 0;
+		background-color: #222;
+		color: #fff;
+		width: 200px;
+		font-family: 'Courier New', Courier, monospace;
+	}
+	.deletebutton {
+		background-color: #f00;
+		color: #fff;
+		float: right;
+	}
 </style>
--- src/StylesheetEditStack.svelte
+++ src/StylesheetEditStack.svelte
@@ -1,47 +1,47 @@
 <script>
 	import StyleEditor from "./StyleEditor.svelte";
-     import { activeEditors, idIncrement } from './store.js';
-     $activeEditors = [];
-     idIncrement.set(0);
-     function makeid(length) {
-          var result           = '';
-          var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-          var charactersLength = characters.length;
-          for ( var i = 0; i < length; i++ ) {
-               result += characters.charAt(Math.floor(Math.random() * charactersLength));
-          }
-          return result;                
-     }
-     function addEditor() {
-          var l = $activeEditors.length;	// get our current items list count
+	import { activeEditors, idIncrement } from './store.js';
+	$activeEditors = [];
+	idIncrement.set(0);
+	function makeid(length) {
+		var result		 = '';
+		var characters	  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+		var charactersLength = characters.length;
+		for ( var i = 0; i < length; i++ ) {
+			result += characters.charAt(Math.floor(Math.random() * charactersLength));
+		}
+		return result;			 
+	}
+	function addEditor() {
+		var l = $activeEditors.length;	// get our current items list count
 		$activeEditors[l] = { id: $idIncrement, name: makeid(25) };
 		$idIncrement++;
-     }
+	}
 </script>
 
 <div>
 
-     <ul>
-          {#each $activeEditors as activeEditor}
-               <StyleEditor {activeEditor}/>
-          {/each}
-     </ul>
+	<ul>
+		{#each $activeEditors as activeEditor}
+			<StyleEditor {activeEditor}/>
+		{/each}
+	</ul>
 
-     <div>
-          <button class="addNewButton" on:click={addEditor}>Add Editor</button>
-     </div>
+	<div>
+		<button class="addNewButton" on:click={addEditor}>Add Editor</button>
+	</div>
 </div>
 
 <style>
-     ul {
-          list-style: none; /* Remove list bullets */
-          padding: 0;
-          margin: 0;
-          margin-bottom: 10px;
-     }
-     .addNewButton {
-          background-color: #0087ff;
-          color: #fff;
-          width: 100%;
-     }
+	ul {
+		list-style: none; /* Remove list bullets */
+		padding: 0;
+		margin: 0;
+		margin-bottom: 10px;
+	}
+	.addNewButton {
+		background-color: #0087ff;
+		color: #fff;
+		width: 100%;
+	}
 </style>
